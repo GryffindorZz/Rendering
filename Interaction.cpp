debeb0df1b6bc507289aeb95937e55978b8bc199
GryffindorZz <18810434164@163.com>
2017-01-09 22:50:52 +0800

    Add interaction

---
+++ Interaction.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Gryffindor on 2017/1/9.
+//
+
---
+++ Interaction.h
@@ -0,0 +1,8 @@
+//
+// Created by Gryffindor on 2017/1/9.
+//
+
+#ifndef RENDERING_INTERACTION_H
+#define RENDERING_INTERACTION_H
+
+#endif //RENDERING_INTERACTION_H
